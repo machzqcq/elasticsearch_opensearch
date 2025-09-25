10260ad6fff3be1c76bf42382835d7ab9027dfef
pradeep <pradeep@seleniumframework.com>
2025-09-25 14:11:14 -0400

    aligning traditional search chapter - initial

---
+++ opensearch/my_tutorial/scripts/2. TRADITIONAL_SEARCH/create_ecommerce_original_edge_ngrams.py
@@ -0,0 +1,44 @@
+from opensearchpy.helpers import bulk
+from opensearchpy import OpenSearch
+
+import pandas as pd
+import json
+
+IS_AUTH = True # Set to True if authentication is required
+HOST = 'localhost' # Replace with your OpenSearch hostname or IP address
+
+if IS_AUTH:
+    # Initialize the OpenSearch client
+    client = OpenSearch(
+        hosts=[{'host': HOST, 'port': 9200}],
+        http_auth=('admin', 'Developer@123'),  # Replace with your credentials
+        use_ssl=True,
+        verify_certs=False,
+        ssl_show_warn=False
+    )
+else:
+    # Initialize the OpenSearch client without authentication
+    client = OpenSearch(
+        hosts=[{'host': HOST, 'port': 9200}],
+        use_ssl=False,
+        verify_certs=False,
+        ssl_assert_hostname = False,
+        ssl_show_warn=False
+    )
+
+BASE_DIR = "../../data"
+# Load the data from the JSON file
+data = []
+with open(f"{BASE_DIR}/ecommerce.json", 'r') as file:
+    for line in file:
+        data.append(line.strip() + '\n')
+
+# Load the index mappings from the JSON file
+with open(f"{BASE_DIR}/ecommerce_edge_ngrams.json", 'r') as file:
+    index_mappings = json.load(file)
+
+# Create the index with the specified mappings
+client.indices.create(index='ecommerce', body=index_mappings, ignore=400)
+
+# Use the bulk helper to index the data
+bulk(client, actions=data, index='ecommerce', refresh=True)
